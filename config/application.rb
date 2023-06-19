fca65c7d4f113947baf9f4d49729a8c2661225a2
Breno Cardoso <elisiobreno@gmail.com>
2023-06-19 14:19:35 -0300

    Change config for allowed hosts

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,5 @@ module F1FantasyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.hosts << Rails.application.secrets.host_url
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << Rails.application.secrets.host_url
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
